0c531c1c510af66aa0f4e7bccb21c0b64f9de73e
Steven (Han) Au <stevenhcau@gmail.com>
2024-06-04 11:36:09 -0600

    added .venv for project, built out function to grab school codes from csv file

--- assignment3/school_data.py
+++ assignment3/school_data.py
@@ -6,28 +6,54 @@
 # You may import any modules from the standard Python library.
 # Remember to include docstrings and comments.
 
-
 import numpy as np
+import csv
 from given_data import year_2013, year_2014, year_2015, year_2016, year_2017, year_2018, year_2019, year_2020, year_2021, year_2022
 
 # Declare any global variables needed to store the data here
+school_codes = dict()
+
+
 
 
 # You may add your own additional classes, functions, variables, etc.
 
+def get_school_codes():
+    
+    school_codes_dict = dict()
+
+    with open('assignment3/Assignment3Data.csv', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+                continue
+            elif row[2] not in school_codes:
+                school_codes_dict[row[2]] = row[1]
+                line_count += 1
+        
+    return school_codes_dict
+
 
 def main():
-    print("ENSF 692 School Enrollment Statistics")
+    
+    school_codes = get_school_codes()
+    print(school_codes)
+    print(year_2013)
 
-    # Print Stage 1 requirements here
+    # print("ENSF 692 School Enrollment Statistics")
 
-    # Prompt for user input
+    # # Print Stage 1 requirements here
 
-    # Print Stage 2 requirements here
-    print("\n***Requested School Statistics***\n")
+    # # Prompt for user input
 
-    # Print Stage 3 requirements here
-    print("\n***General Statistics for All Schools***\n")
+    # # Print Stage 2 requirements here
+    # print("\n***Requested School Statistics***\n")
+
+    # # Print Stage 3 requirements here
+    # print("\n***General Statistics for All Schools***\n")
 
 
 if __name__ == '__main__':
